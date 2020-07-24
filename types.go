5bcdbf0074144b8a859e69f0dc196fd7160fc024
Aditya Diwakar <aditya@diwakar.io>
2020-07-24 15:13:31 -0400

    :package move chart specifics from types -> charts

--- types.go
+++ types.go
@@ -1,25 +1,5 @@
 package flux
 
-import (
-	"fmt"
-)
-
-func (c *ChartRequestSignature) shortName() string {
-	return fmt.Sprintf("%s@%s:%s", c.Ticker, c.Range, c.Width)
-}
-
-// ChartRequestSignature is the parameter for a chart request
-type ChartRequestSignature struct {
-	// ticker for the content of a chart request
-	Ticker string
-
-	// range is the timeframe for chart data to be received, see specs.txt
-	Range string
-
-	// width is the width of the candles to be received, see specs.txt
-	Width string
-}
-
 type protocolPacketData struct {
 	Ver       string `json:"ver"`
 	Fmt       string `json:"fmt"`
@@ -71,67 +51,6 @@ type gatewayRequest struct {
 
 type gatewayRequestLoad struct {
 	Payload []gatewayRequest `json:"payload"`
-}
-
-type keyCachedData struct {
-	Data cachedData `json:"data"`
-}
-
-type cachedData struct {
-	Symbol     string `json:"symbol"`
-	Instrument struct {
-		Symbol                 string `json:"symbol"`
-		RootSymbol             string `json:"rootSymbol"`
-		DisplaySymbol          string `json:"displaySymbol"`
-		RootDisplaySymbol      string `json:"rootDisplaySymbol"`
-		FutureOption           bool   `json:"futureOption"`
-		Description            string `json:"description"`
-		Multiplier             int    `json:"multiplier"`
-		SpreadsSupported       bool   `json:"spreadsSupported"`
-		Tradeable              bool   `json:"tradeable"`
-		InstrumentType         string `json:"instrumentType"`
-		ID                     int    `json:"id"`
-		SourceType             string `json:"sourceType"`
-		IsFutureProduct        bool   `json:"isFutureProduct"`
-		HasOptions             bool   `json:"hasOptions"`
-		Composite              bool   `json:"composite"`
-		FractionalType         string `json:"fractionalType"`
-		DaysToExpiration       int    `json:"daysToExpiration"`
-		SpreadDaysToExpiration string `json:"spreadDaysToExpiration"`
-		Cusip                  string `json:"cusip"`
-		Industry               int    `json:"industry"`
-		Spc                    int    `json:"spc"`
-		ExtoEnabled            bool   `json:"extoEnabled"`
-		Flags                  int    `json:"flags"`
-	} `json:"instrument"`
-	Timestamps []int64   `json:"timestamps"`
-	Open       []float64 `json:"open"`
-	High       []float64 `json:"high"`
-	Low        []float64 `json:"low"`
-	Close      []float64 `json:"close"`
-	Volume     []int     `json:"volume"`
-	Events     []struct {
-		Symbol      string `json:"symbol"`
-		CompanyName string `json:"companyName"`
-		Website     string `json:"website"`
-		IsActual    bool   `json:"isActual"`
-		Time        int64  `json:"time"`
-	} `json:"events"`
-	Service    string `json:"service"`
-	RequestID  string `json:"requestId"`
-	RequestVer int    `json:"requestVer"`
-}
-
-type newChartObject struct {
-	Op    string     `json:"op"`
-	Path  string     `json:"path"`
-	Value cachedData `json:"value"`
-}
-
-type updateChartObject struct {
-	Op    string `json:"op"`
-	Path  string `json:"path"`
-	Value int    `json:"value"`
 }
 
 type heartbeat struct {
